b70f405d76fd24c141137ccfc093b8214a479982
Shakir Sultanov <shkrsltnv@macbookair.home>
2025-01-28 20:14:17 +0100

    Re-deploy again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+  Sanya: string = 'Сань ты пидор, тебя не взламывали';
+  Sanya2: string = 'Та невже';
+  isTextVisible: boolean = false;
+  isTextVisible2: boolean = false;
+  showText() {
+    this.isTextVisible = true;
+  }
+
+  showText2() {
+    this.isTextVisible2 = true;
+  }
+
 }
