7c248b064ff56ea63367d55c9903aacc85792492
Shakir Sultanov <sultanovshakir12@gmail.com>
2025-03-06 12:30:50 +0100

    Projects fixed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -29,7 +29,7 @@ export class HomeComponent implements AfterViewInit, OnInit {
       this.featuredProjects = data.featuredProjects;
     });
 
-    this.http.get<any>('/assets/data/skills.json').subscribe(data => {
+    this.http.get<any>('./assets/data/skills.json').subscribe(data => {
       this.techStack = data.techStack;
     });
   }
