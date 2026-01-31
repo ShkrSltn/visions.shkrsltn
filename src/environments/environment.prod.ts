c770757dfd60f622dc50c70695c2164cfb14a4f9
Shakir Sultanov <sultanovshakir12@gmail.com>
2026-01-31 21:26:49 +0100

    env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  openaiApiKey: '', // Replaced at build time via CI/CD
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  openaiApiKey: '', // Set locally, don't commit with real key
+};
